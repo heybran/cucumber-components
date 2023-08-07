36671af3a638f383bfef4b6a3b53ad7be2e962f3
brandon <gwonzhang@gmail.com>
2023-08-07 16:43:42 +0800

    Import breeze-text-field for manual testing

--- main.js
+++ main.js
@@ -1,22 +1,3 @@
-import "./src/components/select/select.js";
-
-const items = [
-  {
-    label: 'Most recent first',
-    value: 'recent',
-  },
-  {
-    label: 'Rating: high to low',
-    value: 'rating-desc',
-  },
-  {
-    label: 'Rating: low to high',
-    value: 'rating-asc',
-  }
-];
-
-const select = document.createElement('breeze-select');
-select.value = 'demo value';
-select.items = items;
-document.querySelector("#app").append(select);
+import "./src/components/tooltip/tooltip.js";
+import "./src/components/text-field/breeze-text-field.js";
 
