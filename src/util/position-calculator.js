c74f81c4068d6498d03186db1e3a0510e1c71f2f
brandon <gwonzhang@gmail.com>
2023-08-12 23:37:32 +0800

    feat: set tooltip arrow position

--- src/util/position-calculator.js
+++ src/util/position-calculator.js
@@ -14,21 +14,39 @@ export default function calculatePosition({ element, target, position, offset })
   if (position === 'top') {
     y = top - elementHeight - offset;
     x = left + (targetWidth / 2) - (elementWidth / 2);
+    // @ts-ignore
+    if (element.hasArrow) {
+      element.setAttribute('arrow-position', 'bottom');
+    }
     if (y < 2) { // 2 => breathing space
-      y = top + targetHeight + offset;
+      // y = top + targetHeight + offset;
+      return calculatePosition({ element, target, position: 'bottom', offset });
     }
   } else if (position === 'bottom') {
     y = top + targetHeight + offset;
     x = left + (targetWidth / 2) - (elementWidth / 2);
+    // @ts-ignore
+    if (element.hasArrow) {
+      element.setAttribute('arrow-position', 'top');
+    }
     if (y + elementHeight + 2 >  window.innerHeight) {
-      y = top - elementHeight - offset;
+      // y = top - elementHeight - offset;
+      return calculatePosition({ element, target, position: 'top', offset });
     }
   } else if (position === 'left') {
     y = top + (targetHeight / 2) - (elementHeight / 2);
     x = left - offset - elementWidth;
+    // @ts-ignore
+    if (element.hasArrow) {
+      element.setAttribute('arrow-position', 'right');
+    }
   } else if (position === 'right') {
     y = top + (targetHeight / 2) - (elementHeight / 2);
     x = left + targetWidth + offset;
+    // @ts-ignore
+    if (element.hasArrow) {
+      element.setAttribute('arrow-position', 'left');
+    }
   }
 
   return { x, y };
