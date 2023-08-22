6ccefca116638a9302d7371fb4fec8040ce6b179
brandon <gwonzhang@gmail.com>
2023-08-22 20:55:48 +0800

    refactor(breeze-button): add loading and disabled 1. add loading and disabled support 2. fire reset event for reset button 3. move custom properties to button.css

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,27 +1,115 @@
-// @ts-check
-import shared from "../../shared/shared.css?inline";
+import FormElement from "../../shared/form-element.js";
 import css from "./button.css?inline";
 import html from "./button.html?raw";
 
-const template = document.createElement('template');
-template.innerHTML = `<style>${shared}${css}</style>${html}`;
+export default class BreezeButton extends FormElement {
+	get type() {
+		return this.getAttribute("type");
+	}
 
-export default class BreezeButton extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  }
+	connectedCallback() {
+		super.render(html, css);
+		this.loading = this.hasAttribute("loading");
+		this.disabled = this.hasAttribute("disabled");
+		if (this.type) {
+			this.button.setAttribute("type", this.type);
+			this.handleButtonClick(this.type);
+		}
+	}
 
-  connectedCallback() {
-    this.render();
-  }
+	/**
+	 *
+	 * @param {string} type
+	 */
+	handleButtonClick(type) {
+		if (!["submit", "reset"].includes(type)) {
+			return;
+		}
 
-  render() {
-    // @ts-ignore
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-  }
+		const form = this.getForm();
+
+		if (!form) {
+			return console.warn(
+				`breeze-button has type ${type} but no associated form is found`,
+			);
+		}
+
+		this.button.addEventListener("click", (event) => {
+			// FIXME: form got submitted twice
+			form.dispatchEvent(new Event(type, { cancelable: true }));
+		});
+	}
+
+	/**
+	 * @returns {HTMLButtonElement}
+	 */
+	get button() {
+		// @ts-ignore
+		return this.shadowRoot.querySelector("button");
+	}
+
+	render() {
+		// @ts-ignore
+		this.shadowRoot.appendChild(template.content.cloneNode(true));
+	}
+
+	static get observedAttributes() {
+		return ["type", "loading", "disabled"];
+	}
+
+	attributeChangedCallback(attr, oldValue, newValue) {
+		if (null === oldValue) {
+			return;
+		}
+
+		if (attr === "type") {
+			this.button.setAttribute(attr, newValue);
+		} else if (attr === "loading") {
+			this.loading = this.hasAttribute("loading");
+		} else if (attr === "disabled") {
+			this.disabled = this.hasAttribute("disabled");
+		}
+	}
+
+	/**
+	 * @returns {boolean}
+	 */
+	get loading() {
+		return this.hasAttribute("loading");
+	}
+
+	set loading(flag) {
+		if (flag) {
+			import("../spinner/spinner.js").then(({ default: BreezeSpinner }) => {
+				const spinner = document.createElement(BreezeSpinner.is);
+				spinner.setAttribute("part", "spinner");
+				this.button.append(spinner);
+				this.button.setAttribute("aria-busy", "true");
+			});
+		} else {
+			this.button.querySelector('[part="spinner"]')?.remove();
+			this.button.removeAttribute("aria-busy");
+		}
+	}
+
+	/**
+	 * @returns {boolean}
+	 */
+	get disabled() {
+		return this.hasAttribute("disabled");
+	}
+
+	set disabled(flag) {
+		if (flag) {
+			this.button.setAttribute("disabled", "");
+			this.button.setAttribute("aria-disabled", "true");
+		} else {
+			this.button.removeAttribute("disabled");
+			this.button.setAttribute("aria-disabled", "false");
+		}
+	}
 }
 
-if (!customElements.get('breeze-button')) {
-  customElements.define('breeze-button', BreezeButton);
+if (!customElements.get("breeze-button")) {
+	customElements.define("breeze-button", BreezeButton);
 }
