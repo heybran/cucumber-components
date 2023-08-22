0a5c89e91307ca2a893bc1aa992a77777a2f2d1b
brandon <gwonzhang@gmail.com>
2023-08-22 16:30:26 +0800

    refactor(breeze-text-field)
    1. extract html as standalone file
    2. add formdata/reset event

---
+++ src/components/text-field/text-field.js
@@ -0,0 +1,109 @@
+// @ts-check
+import html from "./text-field.html?raw";
+import css from "./text-field.css?inline";
+import FormElement from "../../shared/form-element.js";
+
+export default class BreezeTextField extends FormElement {
+  get input() {
+    // @ts-ignore
+    return this.shadowRoot.querySelector('input');
+  }
+  
+  /**
+   * @param {string} arg
+   */
+  set label(arg) {
+    // @ts-ignore
+    this.shadowRoot.querySelector('[part="label"] span').textContent = arg;
+  }
+
+  /**
+   * @param {string} arg
+   */
+  set placeholder(arg) {
+    // @ts-ignore
+    this.input.placeholder = arg;
+  }
+
+  /**
+   * @param {string} arg
+   */
+  set helperText(arg) {
+    // @ts-ignore
+    this.shadowRoot.querySelector('[part="helper-text"]').textContent = arg;
+  }
+
+  /**
+   * @returns {string}
+   */
+  get value() {
+    // @ts-ignore
+    return this.input.value;
+  }
+
+  /**
+   * @param {string} arg
+   */
+  set value(arg) {
+    // @ts-ignore
+    this.input.value = arg;
+  }
+
+  connectedCallback() {
+    super.render(html, css);
+    if (this.value) {
+      this.value = this.getAttribute('value') ?? '';
+    }
+
+    /**
+     * Store an initial value to be used on form resetting
+     */
+    this._initialValue = this.value;
+
+    if (this.hasAttribute('label')) {
+      this.label = this.getAttribute('label') ?? '';
+    }
+
+    if (this.hasAttribute('placeholder')) {
+      this.placeholder = this.getAttribute('placeholder') ?? '';
+    }
+
+    if (this.hasAttribute('helper-text')) {
+      this.helperText = this.getAttribute('helper-text') ?? '';
+    }
+
+    this.getForm()?.addEventListener('formdata', (event) => {
+      const formData = event.formData;
+      if (this.hasAttribute('name')) {
+        return console.warn(`No 'name' attribute found on ${this.localName}, so this form field will not particiate on form submit.`);
+      }
+      // @ts-ignore
+      formData.set(this.getAttribute('name'), this.value);
+    });
+
+    this.getForm()?.addEventListener('reset', (event) => {
+      // @ts-ignore
+      this.value = this._initialValue;
+    });
+  }
+
+  /**
+   * @returns {string[]}
+   */
+  static get observedAttributes() {
+    return [...super.observedAttributes];
+  }
+
+  /**
+   * @param {string} attr 
+   * @param {string} oldValue 
+   * @param {string} newValue 
+   */
+  attributeChangedCallback(attr, oldValue, newValue) {
+    super.attributeChangedCallback(attr, oldValue, newValue);
+  }
+}
+
+if (!customElements.get('breeze-text-field')) {
+  customElements.define('breeze-text-field', BreezeTextField);
+}
