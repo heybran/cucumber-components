e369857829681418a06082e04409d735980c30ca
brandon <gwonzhang@gmail.com>
2023-08-26 16:31:25 +0800

    feat: add defer and uuid methods

--- src/shared/base-element.js
+++ src/shared/base-element.js
@@ -13,6 +13,22 @@ export default class BaseElement extends HTMLElement {
   }
 
   /**
+   * @param {IdleRequestCallback} callback 
+   * @returns {void}
+   */
+  defer(callback) {
+    const _defer = window.requestIdleCallback || requestAnimationFrame;
+    _defer(callback);
+  }
+
+  /**
+   * @returns {string}
+   */
+  uuid() {
+    return Math.random().toString(36).substring(2, 8);
+  }
+
+  /**
    * @returns {HTMLElement}
    */
   get container() {
