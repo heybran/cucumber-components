263abcf005fed5008872e69c3859c677841a1238
brandon <gwonzhang@gmail.com>
2023-09-06 20:10:46 +0800

    feat: added basic slide features, improvement needed

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -5,7 +5,7 @@ import BaseElement from "../../shared/base-element.js";
 
 export default class CucumberCarousel extends BaseElement {
   /**
-   * @returns {number}
+   * @returns number
    */
   get itemsPerView() {
     let result = 3;
@@ -17,6 +17,13 @@ export default class CucumberCarousel extends BaseElement {
     return result;
   }
 
+  /**
+   * @returns number
+   */
+  get totalSlides() {
+    return Math.ceil(this.carouselItems.length / this.itemsPerView);
+  }
+
   connectedCallback() {
     super.render(html, css);
     // @ts-ignore
@@ -24,7 +31,13 @@ export default class CucumberCarousel extends BaseElement {
     // @ts-ignore
     this.pagination = this.shadowRoot.querySelector('[part="pagination"]');
     // @ts-ignore
-    this.navigationButtons = this.shadowRoot?.querySelectorAll('[part="navigation-button"]');
+    this.navigationButtons = this.shadowRoot.querySelectorAll('[part="navigation-button"]');
+
+    this.style.setProperty('--items-per-view', this.itemsPerView);
+    /**
+     * Default to start with first slide.
+     */
+    this.activeSlide = 1;
 
     if (!this.hasAttribute('aria-label')) {
       this.setAttribute('aria-label', 'Carousel');
@@ -54,13 +67,11 @@ export default class CucumberCarousel extends BaseElement {
    */
   renderPaginationButtons() {
     // @ts-ignore
-    const _count = Math.ceil(this.carouselItems.length / this.itemsPerView);
-    // @ts-ignore
-    for (let i = 0; i < _count; i++) {
+    for (let i = 0; i < this.totalSlides; i++) {
       const button = document.createElement('button');
       button.setAttribute('role', 'tab');
       button.setAttribute('part', 'pagination-button');
-      button.setAttribute('aria-label', `Go to slide ${i+1} of ${_count}`);
+      button.setAttribute('aria-label', `Go to slide ${i+1} of ${this.totalSlides}`);
       if (i === 0) {
         button.setAttribute('aria-selected', 'true');
         button.setAttribute('tabindex', '0');
@@ -68,6 +79,12 @@ export default class CucumberCarousel extends BaseElement {
         button.setAttribute('aria-selected', 'false');
         button.setAttribute('tabindex', '-1');
       }
+      /**
+       * Setting an inline click handler works pretty good here,
+       * since we're inside a for loop already,
+       * downside is that this part 'this.getRootNode().host' doesn't look quite elegant.
+       */
+      button.setAttribute('onclick', `this.getRootNode().host.showSlide(${i+1})`);
       // @ts-ignore
       this.pagination.appendChild(button);
     }
@@ -77,22 +94,108 @@ export default class CucumberCarousel extends BaseElement {
    * @returns void
    */
   showPreviousSlide() {
+    // @ts-ignore
+    this.showSlide(this.activeSlide - 1);
+  }
+
+  
+  /**
+   * @returns void
+  */
+  showNextSlide() {
+    // @ts-ignore
+    this.showSlide(this.activeSlide + 1);
   }
 
   /**
+   * Go to slide by given slide index.
+   * @param {number} slideIndex 
    * @returns void
    */
-  showNextSlide() {
-    // 1. Decrease scroll left
+  showSlide(slideIndex) {
+    // @ts-ignore
     const carouselItemWidth = this.carouselItems[0].clientWidth;
-    const carouselItemCount = this.carouselItems?.length;
     const gap = Number(window.getComputedStyle(this)
       .getPropertyValue('--spacing')
       .replace('px', '').trim());
-    const itemsPerView = 3; // TODO: get it from attribute
-    this.carouselContainer.scrollLeft += (carouselItemWidth + gap) * itemsPerView;
-    // 2. Update pagination
-    // 3. If loop, go to last slide
+
+    // @ts-ignore
+    if (slideIndex > this.activeSlide) {
+      // @ts-ignore
+      this.carouselContainer.scrollLeft += (carouselItemWidth + gap) * this.itemsPerView;
+    } else {
+      // @ts-ignore
+      this.carouselContainer.scrollLeft -= (carouselItemWidth + gap) * this.itemsPerView;
+    }
+
+    this.updatePaginationButtonsStatus(slideIndex);
+
+    this.activeSlide = slideIndex;
+
+    if (!this.hasAttribute('loop')) {
+      if (this.activeSlide === this.totalSlides) {
+        this.disableNextButton();
+      } else if (this.activeSlide === 1) {
+        this.disablePreviousButton();
+      }
+    }
+  }
+
+  /**
+   * Update pagination buttons status by given slide index
+   * @param {number} slideIndex 
+   */
+  updatePaginationButtonsStatus(slideIndex) {
+    const currentButton = this.shadowRoot?.querySelector('[role="tab"][aria-selected="true"]');
+    // @ts-ignore
+    const buttonToUpdated = Array.from(this.pagination.children).find((_, index) => {
+      return index + 1 === slideIndex;
+    });
+
+    // @ts-ignore
+    currentButton.setAttribute('aria-selected', 'false');
+    // @ts-ignore
+    currentButton.setAttribute('tabindex', '-1');
+    buttonToUpdated?.setAttribute('aria-selected', 'true');
+    buttonToUpdated?.setAttribute('tabindex', '0');
+  }
+
+  disablePreviousButton() {
+    this.enableCurrentDisabledButton().disableNavigationButton('previous');
+  }
+
+  disableNextButton() {
+    this.enableCurrentDisabledButton().disableNavigationButton('next');
+  }
+
+  /**
+   * Enable existing disabled navigation button, if any
+   * @returns this
+   */
+  enableCurrentDisabledButton() {
+    // @ts-ignore
+    const currentDisabledButton = this.shadowRoot.querySelector('[part="navigation-button"][disabled]');
+    if (currentDisabledButton) {
+      currentDisabledButton.removeAttribute('disabled');
+      currentDisabledButton.setAttribute('aria-disabled', 'false');
+    }
+    return this;
+  }
+
+  /**
+   * Disable navigation button by given index
+   * @param {'previous'|'next'} type
+   * @returns this
+   */
+  disableNavigationButton(type) {
+    const index = type === 'previous' ? 1 : 2;
+    // @ts-ignore
+    const button = this.shadowRoot.querySelector(`[part="navigation-button"]:nth-child(${index})`);
+    // @ts-ignore
+    button.setAttribute('disabled', '');
+    // @ts-ignore
+    button.setAttribute('aria-disabled', 'true');
+    return this;
   }
 }
 
