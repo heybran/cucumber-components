0f575536cf25f1c3545bcc4d294ae7997b046a2c
brandon <gwonzhang@gmail.com>
2023-09-15 20:50:11 +0800

    fix: port over attributeChangedCallback() logic here

--- src/shared/form-element.js
+++ src/shared/form-element.js
@@ -1,22 +1,44 @@
 // @ts-check
 import BaseElement from "./base-element.js";
 
-const attrs = {
-  password: {
-    required: Boolean,
-    pattern: String,
-    minlength: Number,
-    maxlength: Number,
-  }
-}
-
 export default class FormElement extends BaseElement {
+  constructor() {
+    super();
+    this.attributesDefined = [];
+  }
+
   /**
    * @returns {HTMLInputElement}
    */
   get input() {
     // @ts-ignore
     return this.shadowRoot.querySelector('input');
+  }
+
+  /**
+   * @returns {HTMLTextAreaElement}
+   */
+  get textarea() {
+    // @ts-ignore
+    return this.shadowRoot.querySelector('textarea');
+  }
+
+  /**
+   * @returns {HTMLInputElement|HTMLTextAreaElement}
+   */
+  get reflectTarget() {
+    if (this.textarea) {
+      return this.textarea;
+    }
+
+    return this.input;
+  }
+
+  /**
+   * @returns {string}
+   */
+  get value() {
+    return this.reflectTarget.value;
   }
 
   getForm() {
@@ -80,5 +102,72 @@ export default class FormElement extends BaseElement {
       // @ts-ignore
       formData.set(this.getAttribute("name"), this.value);
     }
-	};
+	}
+
+  /**
+	 * 
+	 * @param {string} attr 
+	 * @param {string|null} oldValue 
+	 * @param {string|null} newValue 
+	 */
+  attributeChangedCallback(attr, oldValue, newValue, FORM_FIELD) {
+		const attrSource = FORM_FIELD[attr];
+		if (!this.attributesDefined.includes(attr)) {
+			Object.defineProperty(this, attr, {
+				get() {
+					if (attrSource.type === Boolean) {
+						return this.hasAttribute(attr);
+					} else if (attrSource.type === String) {
+						/**
+						 * If 'value' is set on host, we define a getter/setter,
+						 * if 'value' is not present, this.value works too as we've defined a getter
+						 * at parent class FromElement.
+						 */
+						if (attr === 'value') {
+							/**
+							 * Getting value from either input/textarea element,
+							 * otherwise we will need to listen for input event on input/textarea element,
+							 * and sync the attribue to host.
+							 */
+							return this.reflectTarget.value;
+						}
+						return this.getAttribute(attr) ?? '';
+					}
+				},
+				set(arg) {
+					if (attrSource.type === Boolean) {
+						this.toggleAttribute(attr, Boolean(arg));
+					} else if (attrSource.type === String) {
+						this.setAttribute(attr, arg);
+					}
+				}
+			});
+
+			this.attributesDefined.push(attr);
+		}
+
+		if (attrSource.reflect) {
+			if (attrSource.type === String) {
+				this.reflectTarget.setAttribute(attr, newValue);
+			} else if (attrSource.type === Boolean) {
+				this.reflectTarget.toggleAttribute(attr, this.hasAttribute(attr));
+			}
+		}
+
+		/**
+		 * If 'label' attribute is changed,
+		 * assuming we have an onLabelChange method, we call this method, 
+		 * pass along newValue into this method, optionally.
+		 */
+		let attrConverted = attr.charAt(0).toUpperCase() + attr.slice(1);
+		if (attr.includes('-')) {
+			const convertedName = attr.replace(/-(\w)/g, (_, letter) => letter.toUpperCase());
+			attrConverted = convertedName.charAt(0).toUpperCase() + convertedName.slice(1);
+		}
+		const methodNameNeeded = `on${attrConverted}Change`;
+
+		if (typeof this[methodNameNeeded] === 'function') {
+			this[methodNameNeeded](newValue);
+		}
+  }
 }
