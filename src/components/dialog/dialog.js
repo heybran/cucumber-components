e3835c874243ffd453b0c55d2bb24192cd2a6274
brandon <gwonzhang@gmail.com>
2023-11-04 15:07:07 +0800

    feat: a11y improvement for dialog (#11)

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -10,8 +10,15 @@ export default class CucumberDialog extends BaseElement {
   /** @type {string} */
   static __localName = 'cc-dialog';
 
+  /**
+   * Functions to run after dialog is closed.
+   * @type {Array<Function>} - callback functions
+   */
+  #after_hide_callbacks = [];
+
   constructor() {
-    super().render(html, css, sharedCSS);
+    super();
+    this.render(html, css, sharedCSS);
   }
 
   /**
@@ -22,40 +29,70 @@ export default class CucumberDialog extends BaseElement {
     return this.shadowRoot.querySelector('dialog');
   }
 
-  #showModal() {
-    this.dialog.showModal();
-    this.#afterShow();
-  }
-
   /**
    * Make another API that also opens dialog.
    * @returns void
    */
   show() {
-    return this.#showModal();
-  }
-
-  /**
-   * Actions to perform after dialog is shown to users.
-   * @returns void
-   */
-  #afterShow() {
-    const custom_initial_focus_element = this.querySelector('[initial-focus]');
-    if (!custom_initial_focus_element) return;
-    // @ts-ignore
-    custom_initial_focus_element?.focus();
+    this.setupA11yDesc();
+    this.dialog.showModal();
+    this.setupInitialFocus();
   }
 
   close() {
-    return this.dialog.close();
+    if (this.#after_hide_callbacks.length > 0) {
+      this.#after_hide_callbacks.forEach((fn) => {
+        fn();
+        this.#after_hide_callbacks = this.#after_hide_callbacks.filter(callback => callback !== fn);
+      });
+    }
+    this.dialog.close();
   }
 
   hide() {
     return this.dialog.close();
   }
 
-  connectedCallback() {
+  /**
+   * Clean up action after dialog is closed
+   * @param {Function} callback 
+   */
+  afterHide(callback) {
+    this.#after_hide_callbacks.push(callback);
   }
+
+  /**
+   * Actions to perform after dialog is shown to users.
+   * @returns void
+   */
+  setupInitialFocus() {
+    const custom_initial_focus_element = this.querySelector('[initial-focus]');
+    if (!custom_initial_focus_element) return;
+    // @ts-ignore
+    custom_initial_focus_element?.focus();
+  }
+
+  /**
+   * Prepare a11y dialog description for screen readers.
+   * @returns void
+   */
+  setupA11yDesc() {
+    const a11y_desc_element = this.querySelector('[a11y-desc]');
+    if (!a11y_desc_element) return;
+
+    const _clone = a11y_desc_element.cloneNode(true);
+    _clone.id = 'dialog-desc';
+    _clone.setAttribute('hidden', '');
+    this.dialog.setAttribute('aria-describedby', 'dialog-desc');
+    this.shadowRoot?.appendChild(_clone);
+
+    this.afterHide(() => {
+      _clone.remove();
+      this.dialog.removeAttribute('aria-describedby');
+    });
+  }
+
+  connectedCallback() {}
 
   static get observedAttributes() {
 		return ['label'];
