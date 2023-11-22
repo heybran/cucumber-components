87d7ac5d2a3054065bc7566491023a16ceabc3a2
brandon <gwonzhang@gmail.com>
2023-11-22 11:18:25 +0800

    refactor: update position calculator script

--- src/util/position-calculator.js
+++ src/util/position-calculator.js
@@ -3,64 +3,138 @@
  * @param {Object} options
  * @param {HTMLElement} options.popover
  * @param {HTMLElement} options.anchorElement
- * @param {'top'|'bottom'|'left'|'right'|'bottom-end'|'bottom-start'} options.position
+ * @param {'top'|'bottom'|'left'|'right'|'top-start'|'top-center'|'top-end'|'bottom-start'|'bottom-center'|'bottom-end'} options.position
  * @param {number} options.offset
+ * @param {boolean} [options.reverse=false] - Track the placement of popover, as we need to update arrow position.
  */
 export default function calculatePosition({
-	popover,
-	anchorElement,
-	position,
-	offset,
+  popover,
+  anchorElement,
+  position,
+  offset,
+  reverse = false,
 }) {
-	const { left, top, width: anchorElementWidth, height: anchorElementHeight } = anchorElement.getBoundingClientRect();
-	let x;
-	let y;
-	const { width: popoverWidth, height: popoverHeight } = popover.getBoundingClientRect();
+  const {
+    left,
+    top,
+    width: anchorElementWidth,
+    height: anchorElementHeight,
+  } = anchorElement.getBoundingClientRect();
+  let x;
+  let y;
+  const { width: popoverWidth, height: popoverHeight } =
+    popover.getBoundingClientRect();
 
-	switch(position) {
-		case 'top':
-			y = top - popoverHeight - offset;
-			x = left + anchorElementWidth / 2 - popoverWidth / 2;
-		
-			if (y < 2) {
-				// 2 => breathing space
-				// y = top + targetHeight + offset;
-				return calculatePosition({ popover, anchorElement, position: "bottom", offset });
-			}
-			break;
+  switch (position) {
+    case "top-center":
+      y = top - popoverHeight - offset;
+      x = left + anchorElementWidth / 2 - popoverWidth / 2;
 
-		case 'left':
-			y = top + anchorElementHeight / 2 - popoverHeight / 2;
-			x = left - offset - popoverWidth;
-			break;
+      if (y < 2) {
+        // 2 => breathing space
+        // y = top + targetHeight + offset;
+        return calculatePosition({
+          popover,
+          anchorElement,
+          position: "bottom-center",
+          offset,
+          reverse: true,
+        });
+      }
+      break;
 
-		case 'bottom':
-			y = top + anchorElementHeight + offset;
-			x = left - ((popoverWidth - anchorElementWidth) / 2);
-			// x = left;
-			console.log(x, left, popoverWidth, anchorElementWidth);
-		
-			if (y + popoverHeight + 2 > window.innerHeight) {
-				// y = top - elementHeight - offset;
-				return calculatePosition({ popover, anchorElement, position: "top", offset });
-			}
-			break;
+    case "left":
+      y = top + anchorElementHeight / 2 - popoverHeight / 2;
+      x = left - offset - popoverWidth;
+      break;
 
-		case 'right':
-			y = top + anchorElementHeight / 2 - popoverHeight / 2;
-			x = left + anchorElementWidth + offset;
-			break;
+    case "bottom-center":
+      y = top + anchorElementHeight + offset;
+      x = left - (popoverWidth - anchorElementWidth) / 2;
 
-		case "bottom-start":
-			y = top + anchorElementHeight + offset;
-			x = left;
-			break;
+      if (y + popoverHeight + 2 > window.innerHeight) {
+        // y = top - elementHeight - offset;
+        return calculatePosition({
+          popover,
+          anchorElement,
+          position: "top-center",
+          offset,
+          reverse: true,
+        });
+      }
+      break;
 
-		case 'bottom-end':
-			y = top + anchorElementHeight + offset;
-			x = left - ((popoverWidth - anchorElementWidth));
-			break;
-	}
+    case "right":
+      y = top + anchorElementHeight / 2 - popoverHeight / 2;
+      x = left + anchorElementWidth + offset;
+      break;
 
-	return { x, y, anchorElementWidth };
+    case "top-start":
+      y = top - popoverHeight - offset;
+      x = left;
+
+      if (y < 2) {
+        // 2 => breathing space
+        // y = top + targetHeight + offset;
+        return calculatePosition({
+          popover,
+          anchorElement,
+          position: "bottom-start",
+          offset,
+          reverse: true,
+        });
+      }
+      break;
+
+    case "bottom-start":
+      y = top + anchorElementHeight + offset;
+      x = left;
+
+      if (y + popoverHeight + 2 > window.innerHeight) {
+        // y = top - elementHeight - offset;
+        return calculatePosition({
+          popover,
+          anchorElement,
+          position: "top-start",
+          offset,
+          reverse: true,
+        });
+      }
+      break;
+
+    case "top-end":
+      y = top - popoverHeight - offset;
+      x = left - (popoverWidth - anchorElementWidth);
+
+      if (y < 2) {
+        // 2 => breathing space
+        // y = top + targetHeight + offset;
+        return calculatePosition({
+          popover,
+          anchorElement,
+          position: "bottom-end",
+          offset,
+          reverse: true,
+        });
+      }
+      break;
+
+    case "bottom-end":
+      y = top + anchorElementHeight + offset;
+      x = left - (popoverWidth - anchorElementWidth);
+
+      if (y + popoverHeight + 2 > window.innerHeight) {
+        // y = top - elementHeight - offset;
+        return calculatePosition({
+          popover,
+          anchorElement,
+          position: "top-end",
+          offset,
+          reverse: true,
+        });
+      }
+      break;
+  }
+
+  return { x, y, anchorElementWidth, reverse };
 }
