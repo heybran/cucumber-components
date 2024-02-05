cf5e62cac8fe73f6d432fbe0447ec3811b90150d
worksapce <289582304@qq.com>
2024-02-05 12:03:38 +0800

    feat: add customise size in space

--- src/components/space/space.js
+++ src/components/space/space.js
@@ -9,14 +9,54 @@ export default class CucumberSpace extends FormElement {
     this.render(html, css, sharedCss);
   }
 
-  get size() {
-    return this.getAttribute("size");
+  /**
+   * @description size of the space.
+   * `small` is 8px, 'middle' is 16px, 'large' is 24px
+   * `number` is custom size
+   * @returns {number} size in pixel
+   */
+  get sizeInPx() {
+    const attrSize = this.getAttribute("size") || 16;
+    if (attrSize === 'small') return 8;
+    if (attrSize === 'large') return 24;
+    if (attrSize === 'middle') return 16;
+    const parsedNum= parseInt(attrSize, 10);
+    return Number.isNaN(parsedNum) ? 16 : parsedNum;
+  }
+
+  /**
+   * horizontal by default if no direction is set
+   * @returns {'horizontal'|'vertical'}
+   */
+  get getDirection() {
+    return this.getAttribute("direction") || "horizontal";
+  }
+
+  // watch the direction and size attribute
+  static get observedAttributes() {
+    return ['direction', 'size'];
+  }
+
+  /**
+   * @description watch for the direction and size attribute changes
+   * @param attr {string} attribute name
+   */
+  attributeChangedCallback(attr) {
+    const sectionRef = this.shadowRoot.querySelector('.cc-space-section');
+    if (sectionRef === null) return;
+    if (attr === 'direction' || attr === 'size') {
+      sectionRef.style.gap = this.getDirection === 'vertical' ? `${this.sizeInPx}px 0` : `0 ${this.sizeInPx}px`;
+    }
   }
 
   connectedCallback() {
-    this._connected = true;
-    this.setctionRef = this.shadowRoot.querySelector(".cc-space");
-    this.setctionRef.style.columnGap = this.size + 'px';
+    const gapProp = this.getDirection === "vertical" ? 'row-gap' : 'column-gap'
+    // render the component
+    super.render(`
+    <section class="cc-space-section" style="${gapProp}: ${this.sizeInPx}px">
+        <slot></slot>
+    </section>
+    `, css, sharedCss);
   }
 }
 
